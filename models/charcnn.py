a5d3fe108102644f31ae412471c473cdf37c934b
Sasha <q.dev.null@gmail.com>
2020-04-14 22:29:50 +0200

    Add the simple convolutional classifier

---
+++ models/charcnn.py
@@ -0,0 +1,44 @@
+import torch
+
+
+class ConvClassifier(torch.nn.Module):
+    def __init__(self, vocab_size, emb_dim, filters_count):
+        super().__init__()
+
+        self._embedding = torch.nn.Embedding(vocab_size, emb_dim)
+        self._conv = torch.nn.Conv2d(1, filters_count, (3, 1))
+        self._dropout = torch.nn.Dropout(0.2)
+        self._relu = torch.nn.ReLU()
+        self._max_pooling = torch.nn.MaxPool2d(kernel_size=(1, 15))
+        self._out_layer = torch.nn.Linear(filters_count * 15, 1, bias=False)
+
+    def forward(self, inputs):
+        '''
+        inputs - LongTensor with shape (batch_size, max_word_len)
+        outputs - FloatTensor with shape (batch_size,)
+        '''
+
+        outputs = self.embedding(inputs)
+        outputs = self._conv(outputs)
+        outputs = self._dropout(outputs)
+        outputs = self._relu(outputs)
+        outputs = self._max_pooling(outputs).reshape(inputs.shape[0], -1)
+        outputs = self._out_layer(outputs)
+        return outputs
+
+    def embedding(self, inputs):
+        batch_size, max_len = inputs.shape
+        embed = self._embedding(inputs).view(batch_size, 1, max_len, -1)
+        return embed
+
+    def get_filters(self):
+        return self._conv.weight.data.cpu().detach().numpy()
+
+
+def main():
+    model = ConvClassifier()
+    print(model)
+
+
+if __name__ == '__main__':
+    main()
