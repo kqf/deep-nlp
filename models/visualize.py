e4a2ee1df61be88e84d7e21a0eb7f2a279d37c32
Sasha <q.dev.null@gmail.com>
2020-04-19 08:00:38 +0200

    Add the character level visualisations

--- models/visualize.py
+++ models/visualize.py
@@ -7,17 +7,17 @@ from sklearn.manifold import TSNE
 from sklearn.preprocessing import scale
 
 
-def draw_vectors(x, y, radius=10, alpha=0.25, color='blue',
+def draw_vectors(x, y, radius=10, alpha=0.25, color="blue",
                  width=600, height=400, show=True, **kwargs):
     output_notebook()
 
     if isinstance(color, str):
         color = [color] * len(x)
     data_source = bm.ColumnDataSource(
-        {'x': x, 'y': y, 'color': color, **kwargs})
+        {"x": x, "y": y, "color": color, **kwargs})
 
-    fig = pl.figure(active_scroll='wheel_zoom', width=width, height=height)
-    fig.scatter('x', 'y', size=radius, color='color',
+    fig = pl.figure(active_scroll="wheel_zoom", width=width, height=height)
+    fig.scatter("x", "y", size=radius, color="color",
                 alpha=alpha, source=data_source)
 
     fig.add_tools(bm.HoverTool(
@@ -35,3 +35,22 @@ def get_tsne_projection(word_vectors):
 def visualize_embeddings(embeddings, token, colors):
     tsne = get_tsne_projection(embeddings)
     draw_vectors(tsne[:, 0], tsne[:, 1], color=colors, token=token)
+
+
+from IPython.core.display import HTML
+import matplotlib.pyplot as plt
+
+
+def get_color_hex(weight):
+    cmap = plt.get_cmap("RdYlGn")
+    rgba = cmap(weight, bytes=True)
+    return "#%02X%02X%02X" % rgba[:3]
+
+
+def visualize(word, weights):
+    symb_template = '<span style="background-color: {color_hex}">{symb}</span>'
+    res = ""
+    for symb, weight in zip(word, weights):
+        res += symb_template.format(color_hex=get_color_hex(weight), symb=symb)
+    res = "<p>" + res + "</p>"
+    return HTML(res)
