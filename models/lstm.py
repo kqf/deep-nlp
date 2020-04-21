31fb3c62fdf92101c522990cdd4d2ef029c30e18
Sasha <q.dev.null@gmail.com>
2020-04-21 17:58:07 +0200

    Fix the CharLSTMClassifier structure

--- models/lstm.py
+++ models/lstm.py
@@ -1,12 +1,11 @@
-import math
-# import time
+import time
 import torch
 import numpy as np
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 # from sklearn.metrics import classification_report
 
 
@@ -24,7 +23,7 @@ def data(filename="data/surnames-multilang.txt"):
     return pd.read_csv(filename, sep="\t", names=["surname", "label"])
 
 
-class MemorizerModel(torch.nn.Module):
+class LSTMModel(torch.nn.Module):
     def __init__(self, hidden_size):
         super().__init__()
 
@@ -49,45 +48,6 @@ class MemorizerModel(torch.nn.Module):
         return result
 
 
-class BasicRNNClassifier():
-    def __init__(self, hidden_size=100, batch_size=25, epochs_count=1):
-        self.hidden_size = hidden_size
-        self.batch_size = batch_size
-        self.epochs_count = epochs_count
-
-    def fit(self, X, y):
-        X = np.array(X)
-        y = np.array(y)
-        rnn = MemorizerModel(hidden_size=self.hidden_size)
-        criterion = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(
-            filter(lambda p: p.requires_grad, rnn.parameters()))
-
-        total_loss = 0
-        indices = np.arange(len(X))
-        np.random.shuffle(indices)
-        batchs_count = int(math.ceil(len(X) / self.batch_size))
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-        for epoch_ind in range(self.epochs_count):
-            for batch_indices in np.array_split(indices, batchs_count):
-                X_batch, y_batch = X[batch_indices], y[batch_indices]
-                batch = torch.LongTensor(X_batch).to(device)
-                labels = torch.LongTensor(y_batch).to(device)
-
-                optimizer.zero_grad()
-                rnn.train()
-
-                logits = rnn(batch)
-
-                loss = criterion(logits, labels)
-                loss.backward()
-                optimizer.step()
-
-                total_loss += loss.item()
-        return self
-
-
 class Tokenizer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         chars = set("".join(X))
@@ -103,6 +63,76 @@ class Tokenizer(BaseEstimator, TransformerMixin):
             padded = np.pad(cc, (0, self.max_len - len(cc)), mode="constant")
             padded_data.append(padded)
         return np.array(padded_data)
+
+
+class CharLSTMClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, lr=0.01, batch_size=32, epochs_count=1, verbose=True):
+        self.batch_size = batch_size
+        self.epochs_count = epochs_count
+        self.verbose = verbose
+        self.model = None
+        self.lr = lr
+        self.clsses_ = [0, 1]
+
+    @staticmethod
+    def batches(X, y, batch_size):
+        num_samples = X.shape[0]
+
+        indices = np.arange(num_samples)
+        np.random.shuffle(indices)
+
+        for start in range(0, num_samples, batch_size):
+            end = min(start + batch_size, num_samples)
+
+            batch_idx = indices[start: end]
+            yield X[batch_idx], y[batch_idx]
+
+    def fit(self, X, y):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.model = LSTMModel(1000)
+
+        optimizer = torch.optim.Adam(
+            [param for param in self.model.parameters()
+             if param.requires_grad],
+            lr=self.lr)
+
+        loss_function = torch.nn.CrossEntropyLoss()
+        for epoch in range(self.epochs_count):
+            batches = self.batches(X, y, self.batch_size)
+            epoch_loss = 0
+            time_epoch = time.time()
+            for i, (X_batch, y_batch) in enumerate(batches):
+                Xt = torch.LongTensor(X_batch).to(device)
+                yt = torch.LongTensor(y_batch).to(device)
+
+                logits = self.model.forward(Xt)
+                loss = loss_function(logits, yt)
+                epoch_loss += loss.item()
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            if self.verbose:
+                te = time.time() - time_epoch
+                print(f"Epoch {epoch}, loss {epoch_loss}, time {te:.2f}")
+        return self
+
+    def predict_proba(self, X):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        X = torch.LongTensor(X).to(device)
+        logits = self.model(X)
+        return torch.sigmoid(logits).cpu().data.numpy()
+
+    def embeddings(self, X):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        X = torch.LongTensor(X).to(device)
+        return self.model.embed(X).cpu().data.numpy()
+
+    def predict(self, X):
+        self.model.eval()
+        return self.predict_proba(X).argmax(axis=1)
 
 
 def build_model(**kwargs):
