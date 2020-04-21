37805d28668e395d6de39190d240d0360d9cad89
Sasha <q.dev.null@gmail.com>
2020-04-21 08:42:31 +0200

    Add the missing tokenizer for LSTM module

--- models/lstm.py
+++ models/lstm.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.collections import Counter
 # from sklearn.metrics import classification_report
 
 
@@ -91,6 +92,37 @@ class BasicRNNClassifier():
 
                 total_loss += loss.item()
         return self
+
+
+class Tokenizer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        chars = set("".join(X))
+        self.c2i = {c: i + 1 for i, c in enumerate(chars)}
+        self.c2i['<pad>'] = 0
+
+        word_len_counter = Counter(list(map(len, X)))
+
+        threshold = 0.99
+        self.max_len = self._find_max_len(word_len_counter, threshold)
+        return self
+
+    @staticmethod
+    def _find_max_len(counter, threshold):
+        sum_count = sum(counter.values())
+        cum_count = 0
+        for i in range(max(counter)):
+            cum_count += counter[i]
+            if cum_count > sum_count * threshold:
+                return i
+        return max(counter)
+
+    def transform(self, X):
+        shorted_data = []
+        for word in X:
+            cc = np.array([self.c2i.get(s, 0) for s in word[:self.max_len]])
+            padded = np.pad(cc, (0, self.max_len - len(cc)), mode="constant")
+            shorted_data.append(padded)
+        return np.array(shorted_data)
 
 
 def build_model(**kwargs):
