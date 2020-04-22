85fe92a5f895753a3b68f72872b2c8b48608ac61
Sasha <q.dev.null@gmail.com>
2020-04-22 09:52:28 +0200

    Remove the baseline solution from charRNN example pipeline

--- models/charrnn.py
+++ models/charrnn.py
@@ -1,28 +1,15 @@
 import math
-# import time
 import torch
 import numpy as np
-import pandas as pd
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
 
 
 """
 !mkdir -p data
-!pip3 -qq install torch
-!pip install -qq bokeh
-!pip install -qq eli5
+!pip -qq install torch
 !pip install scikit-learn
-!curl -k -L "https://drive.google.com/uc?export=download&id=1ji7dhr9FojPeV51dDlKRERIqr3vdZfhu" -o data/surnames-multilang.txt
+!pip install matplotlib
+!pip install seaborn
 """  # noqa
-
-
-def data(filename="data/surnames-multilang.txt"):
-    return pd.read_csv(filename, sep="\t", names=["surname", "label"])
 
 
 class SimpleRNN(torch.nn.Module):
@@ -114,32 +101,8 @@ class BasicRNNClassifier():
         return self
 
 
-def baseline(X_tr, X_te, y_tr, y_te):
-    model = make_pipeline(
-        CountVectorizer(analyzer='char', ngram_range=(1, 4)),
-        LogisticRegression(),
-    )
-    model.fit(X_tr, y_tr)
-
-    print("Train")
-    print(classification_report(y_tr, model.predict(X_tr)))
-
-    print("Test")
-    print(classification_report(y_te, model.predict(X_te)))
-    return model
-
-
 def main():
-    df = data()
-    print(df)
-    le = LabelEncoder()
-    X, y = df["surname"], le.fit_transform(df["label"])
-    X_tr, X_te, y_tr, y_te = train_test_split(
-        X, y, test_size=0.3, stratify=y, random_state=42
-    )
-
-    base = baseline(X_tr, X_te, y_tr, y_te)
-    print(base)
+    pass
 
 
 if __name__ == '__main__':
