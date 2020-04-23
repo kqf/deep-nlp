d48f7aace18cfd7e847c3cd8cb3c13aada5c5d61
Sasha <q.dev.null@gmail.com>
2020-04-23 11:07:41 +0200

    Fix the tests for CharRNN

--- tests/test_charrn.py
+++ tests/test_charrn.py
@@ -10,23 +10,26 @@ np.random.seed(0)
 
 
 @pytest.fixture
-def batch(seq_len=5, batch_size=128):
-    X = torch.randint(0, 10, size=(seq_len, batch_size, 1), dtype=torch.long)
-    return X
+def n_classes():
+    return 10
+
+
+@pytest.fixture
+def batch(n_classes, seq_len=5, batch_size=128):
+    size = (seq_len, batch_size, 1)
+    return torch.randint(0, n_classes, size=size, dtype=torch.long)
 
 
 def test_simple_model(batch, hidden_size=3):
     model = SimpleRNNModel(batch.shape[-1], hidden_size)
-
     logits = model(batch.float())
-    logits.shape == (batch.shape[0], hidden_size)
+    assert logits.shape == (batch.shape[1], hidden_size)
 
 
-def test_memorizer_model(batch, hidden_size=3):
-    model = MemorizerModel(embedding_size=10, hidden_size=hidden_size)
+def test_memorizer_model(batch, n_classes, hidden_size=3):
+    model = MemorizerModel(embedding_size=n_classes, hidden_size=hidden_size)
     logits = model(batch)
-    # It should have hidden_size = output_size
-    logits.shape == (batch.shape[0], hidden_size)
+    assert logits.shape == (batch.shape[1], n_classes)
 
 
 @pytest.fixture
