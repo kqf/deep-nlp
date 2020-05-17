333dba55249994ec4134cd89cb9dab7f6d641edd
Sasha <q.dev.null@gmail.com>
2020-05-17 09:21:55 +0200

    Add the generic text preprocessor

--- models/dialogue.py
+++ models/dialogue.py
@@ -1,5 +1,9 @@
 import os
 import pandas as pd
+from torchtext.data import LabelField, Field, Example, Dataset
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# from torchtext.data import Dataset, BucketIterator
 
 
 """
@@ -28,6 +32,32 @@ def data():
         read_single("data/SlotGated-SLU/data/atis/test/"),
         read_single("data/SlotGated-SLU/data/atis/valid/"),
     )
+
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, fields, min_freq=3):
+        self.min_freq = min_freq
+        self.fields = fields
+
+    def fit(self, X, y=None):
+        dataset = self.transform(X, y)
+        for (_, field) in self.fields:
+            field.build_vocab(dataset, min_freq=self.min_freq)
+        return self
+
+    def transform(self, X, y=None):
+        cols = [X[col].apply(field.preprocess) for col, field in self.fields]
+        examples = [Example.fromlist(c, self.fields) for c in zip(cols)]
+        return Dataset(examples, self.fields)
+
+
+def build_preprocessor():
+    fields = [
+        ('tokens', Field()),
+        ('tags', Field(unk_token=None)),
+        ('intent', LabelField()),
+    ]
+    return TextPreprocessor(fields)
 
 
 def main():
