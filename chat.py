1995069c2eb9fc134573ec8d23777d4483bfca03
Sasha <q.dev.null@gmail.com>
2020-05-25 22:11:28 +0200

    Add the vector builder

--- chat.py
+++ chat.py
@@ -1,5 +1,8 @@
 import spacy
+import numpy as np
 import pandas as pd
+
+from collections import Counter
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import make_union
 
@@ -31,6 +34,41 @@ def build_tokenizer(col1, col2):
         Tokenizer(col2),
     )
     return texts
+
+
+def build_word_embeddings(data, w2v_model, min_freq=5):
+    words = Counter()
+
+    for text in data.question:
+        for word in text:
+            words[word] += 1
+
+    for options in data.options:
+        for text in options:
+            for word in text:
+                words[word] += 1
+
+    word2ind = {
+        '<pad>': 0,
+        '<unk>': 1
+    }
+
+    embeddings = [
+        np.zeros(w2v_model.vectors.shape[1]),
+        np.zeros(w2v_model.vectors.shape[1])
+    ]
+
+    for word, count in words.most_common():
+        if count < min_freq:
+            break
+
+        if word not in w2v_model.vocab:
+            continue
+
+        word2ind[word] = len(word2ind)
+        embeddings.append(w2v_model.get_vector(word))
+
+    return word2ind, np.array(embeddings)
 
 
 def main():
