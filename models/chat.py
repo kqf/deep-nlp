681946bc4e788036bee7813bec0a9a3f26699d1e
Sasha <q.dev.null@gmail.com>
2020-05-31 12:37:40 +0200

    Add the column transformation for the chat data

--- models/chat.py
+++ models/chat.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from torchtext.data import Field, Example, Dataset, BucketIterator
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
 def read_data(filename):
@@ -15,6 +17,33 @@ def data():
         read_data("data/test.txt"),
         read_data("data/valid.txt"),
     )
+
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, fields, need_vocab=None):
+        self.fields = fields
+        self.need_vocab = need_vocab or {}
+
+    def fit(self, X, y=None):
+        dataset = self.transform(X, y)
+        for field, min_freq in self.need_vocab.items():
+            field.build_vocab(dataset, min_freq=min_freq)
+        return self
+
+    def transform(self, X, y=None):
+        proc = [X[col].apply(f.preprocess) for col, f in self.fields]
+        examples = [Example.fromlist(f, self.fields) for f in zip(*proc)]
+        dataset = Dataset(examples, self.fields)
+        return dataset
+
+
+def build_preprocessor():
+    text_field = Field(lower=True)
+    fields = [
+        ('query', text_field),
+        ('target', text_field),
+    ]
+    return TextPreprocessor(fields, need_vocab={text_field: 5})
 
 
 def main():
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,23 +1,30 @@
 import pytest
 import pandas as pd
+from models.chat import build_preprocessor
+from torchtext.data import BucketIterator
 
 
 @pytest.fixture
-def data():
+def data(size=128):
     return pd.DataFrame({
         "query": [
             "How are you?",
             "I am fine as well, and where do you live?",
             "Do you like ice cream?",
-        ],
-        "target:": [
+        ] * size,
+        "target": [
             "I am fine thanks, and you?",
             "Good, I live in Lodon.",
             "No",
-        ]
+        ] * size
 
     })
 
 
-def test_dummy(data):
+def test_dummy(data, batch_size=128):
     print(data)
+    dataset = build_preprocessor().fit_transform(data)
+    data_iter = BucketIterator(dataset, batch_size=batch_size)
+
+    assert next(iter(data_iter)).query.shape[1] == batch_size
+    assert next(iter(data_iter)).target.shape[1] == batch_size
