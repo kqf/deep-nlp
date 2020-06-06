c810daf47783fd0a987ea7bac58cb1db9d13fabb
Sasha <q.dev.null@gmail.com>
2020-06-06 22:53:57 +0200

    Add positional encoding

--- models/attention.py
+++ models/attention.py
@@ -126,6 +126,25 @@ class Decoder(torch.nn.Module):
         return self._out(outputs), hidden
 
 
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2, dtype=torch.float) * (-np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
 class TranslationModel(torch.nn.Module):
     def __init__(
             self,
@@ -170,6 +189,7 @@ def build_model():
         module=TranslationModel,
         optimizer=torch.optim.Adam,  # <<< unexpected
         criterion=torch.nn.CrossEntropyLoss,
+        max_epochs=2,
         batch_size=32,
         iterator_train=SkorchBucketIterator,
         iterator_train__shuffle=True,
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,9 +1,11 @@
+import torch
 import pytest
 import pandas as pd
 
 from torchtext.data import BucketIterator
 
 from models.attention import build_preprocessor, build_model
+from models.attention import PositionalEncoding
 
 
 @pytest.fixture
@@ -22,6 +24,13 @@ def test_textpreprocessor(data, batch_size=32):
     # assert tp.transform(data) is not None
     assert batch.source.shape[0] == batch_size
     assert batch.target.shape[0] == batch_size
+
+
+def test_positional_encoding(emb_dim=10):
+    pe = PositionalEncoding(emb_dim, dropout=0)
+
+    emb = torch.zeros((1, 100, emb_dim))
+    assert pe(emb).shape == emb.shape
 
 
 def test_full_model(data):
