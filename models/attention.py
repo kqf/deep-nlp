10aaf4634329ff2d811b1b332715cf126c9cafe9
Sasha <q.dev.null@gmail.com>
2020-06-06 23:11:10 +0200

    Add the decoder layer for the attention model

--- models/attention.py
+++ models/attention.py
@@ -211,6 +211,30 @@ class EncoderBlock(torch.nn.Module):
         return self._feed_forward_block(outputs, self._feed_forward)
 
 
+class DecoderLayer(torch.nn.Module):
+    def __init__(self, size, self_attn, encoder_attn,
+                 feed_forward, dropout_rate):
+        super().__init__()
+
+        self._self_attn = self_attn
+        self._encoder_attn = encoder_attn
+        self._feed_forward = feed_forward
+        self._self_attention_block = ResidualBlock(size, dropout_rate)
+        self._attention_block = ResidualBlock(size, dropout_rate)
+        self._feed_forward_block = ResidualBlock(size, dropout_rate)
+
+    def forward(self, inputs, encoder_output, source_mask, target_mask):
+        outputs = self._self_attention_block(
+            inputs, lambda inputs: self._self_attn(
+                inputs, inputs, inputs, target_mask)
+        )
+        outputs = self._attention_block(
+            outputs, lambda inputs: self._encoder_attn(
+                inputs, encoder_output, encoder_output, source_mask)
+        )
+        return self._feed_forward_block(outputs, self._feed_forward)
+
+
 class TranslationModel(torch.nn.Module):
     def __init__(
             self,
