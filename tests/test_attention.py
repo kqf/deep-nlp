9833b26eedfabeb75be6af645e1b8f2685452b1d
Sasha <q.dev.null@gmail.com>
2020-06-09 09:19:59 +0200

    Fix the tests for the decoder module

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -47,14 +47,17 @@ def test_encoder(seq_size=100, batch_size=128, vocab_size=30, model_d=10):
     assert outputs.shape == (batch_size, seq_size, model_d)
 
 
-def test_decoder(seq_size=100, batch_size=128, vocab_size=30, model_d=10):
-    dec = Decoder(vocab_size, model_d, 10, 10, 5, 0.7)
-    inputs = torch.randint(0, vocab_size, (batch_size, seq_size))
-    encoder_output = torch.randint(
-        0, vocab_size, (batch_size, seq_size, model_d))
+def test_decoder(
+        batch_size=128, model_d=10,
+        src_seq_size=200, trg_seq_size=100, trg_vocab_size=40):
+    dec = Decoder(trg_vocab_size, model_d, 10, 10, 5, 0.7)
+
+    inp_trg = torch.randint(0, trg_vocab_size, (batch_size, trg_seq_size))
+    enc_src = torch.rand(batch_size, src_seq_size, model_d)
     mask = None
-    outputs = dec(inputs, encoder_output, mask, mask)
-    assert outputs.shape == (batch_size, seq_size, model_d)
+
+    outputs = dec(inp_trg, enc_src, mask, mask)
+    assert outputs.shape == (batch_size, trg_seq_size, trg_vocab_size)
 
 
 # @pytest.mark.skip
