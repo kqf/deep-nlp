9b4a2a2159d29816f36c4a73bf865723c331b46f
Sasha <q.dev.null@gmail.com>
2020-07-10 09:43:52 +0200

    Add the field preprocessort for the LSTM module

--- models/lstm.py
+++ models/lstm.py
@@ -15,6 +15,7 @@ from sklearn.metrics import f1_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
+from torchtext.data import Dataset, Example, Field, LabelField
 
 SEED = 137
 
@@ -46,6 +47,24 @@ def timer(name):
 
 def data(filename="data/surnames-multilang.txt"):
     return pd.read_csv(filename, sep="\t", names=["surname", "label"])
+
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, fields, min_freq=1):
+        self.fields = fields
+        self.min_freq = min_freq
+
+    def fit(self, X, y=None):
+        dataset = self.transform(X, y)
+        for name, field in dataset.fields.items():
+            if field.use_vocab:
+                field.build_vocab(dataset, min_freq=self.min_freq)
+        return self
+
+    def transform(self, X, y=None):
+        proc = [X[col].apply(f.preprocess) for col, f in self.fields]
+        examples = [Example.fromlist(f, self.fields) for f in zip(*proc)]
+        return Dataset(examples, self.fields)
 
 
 class SimpleRNNModel(torch.nn.Module):
